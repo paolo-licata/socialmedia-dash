390e23004ec5241171b75f16dc53cf2ae2fe9acc
paolo-licata <paolo_licata@outlook.com>
2025-03-03 15:36:51 +0100

    Implemented all the services useful for the project

--- src/services/postServices.js
+++ src/services/postServices.js
@@ -25,3 +25,59 @@ export const createPost = async (postData, token) => {
         throw new Error(error.response?.data?.message || "Failed to create post");
     }
 }
+
+//Delete a post
+export const deletePost = async (postId, token) => {
+    try {
+        const response = await axios.delete(`${API_URL}/${postId}`, {
+            headers: {
+                Authorization: `Bearer ${token}`,
+            }
+        })
+        return response.data;
+    } catch (error) {
+        throw new Error(error.response?.data?.message || "Failed to delete post");
+    }
+}
+
+// Adding a comment to a post
+export const addComment = async (postId, commentData, token) => {
+    try {
+        const response = await axios.post(`${API_URL}/${postId}/comments`, commentData, {
+            headers: {
+               Authorization: `Bearer ${token}`, 
+            }
+        });
+        return response.data;
+    } catch (error) {
+        throw new Error(error.response?.data?.message || "Failed to add comment");
+    }
+}
+
+// Deleting a comment from a post
+export const deleteComment = async (postId, commentId, token) => {
+    try {
+        const response = await axios.delete(`${API_URL}/${postId}/comments/${commentId}`, {
+            headers: {
+                Authorization: `Bearer ${token}`,
+            }
+        });
+        return response.data;
+    } catch (error) {
+        throw new Error(error.response?.data?.message || "Failed to delete comment");
+    }
+}
+
+//Like or unlike a post
+export const likePost = async (postId, token) => {
+    try {
+        const response = await axios.post(`${API_URL}/${postId}/likes`, {}, {
+            headers: {
+                Authorization: `Bearer ${token}`,
+            }
+        })
+        return response.data;
+    } catch (error) {
+        throw new Error(error.response?.data?.message || "Failed to like/unlike post")
+    }
+}
