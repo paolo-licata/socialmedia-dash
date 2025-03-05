8ad9c62307e0de07e5815942cd9e9386c696beaf
paolo-licata <paolo_licata@outlook.com>
2025-03-05 19:20:08 +0100

    First layout of LoginForm component

---
+++ src/components/LoginForm.jsx
@@ -0,0 +1,52 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { loginUser } from "../services/authServices";
+
+const LoginForm = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState("null");
+    const navigate = useNavigate();
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        setError("");
+    
+
+        try {
+            const data = await loginUser({ email, password }); // Call API Service
+            localStorage.setItem("token", data.token);
+            navigate("/dashboard");
+        } catch (error) {
+            setError(error.message || "Login failed. Please try again.");
+        }
+    }
+
+    return (
+        <div className="login-container">
+            <h2>Login</h2>
+            {error && <p className="error-message">{error}</p>}
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="email"
+                    placeholder="Enter you email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    required
+                />
+                <input
+                    type="password"
+                    placeholder="Enter your password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    required
+                />
+                <button type="submit">Login</button>
+            </form>
+            <p>Don't have an account yet? <a href="/signup">Sign up!</a></p>
+        </div>        
+      );
+
+}
+
+export default LoginForm;
