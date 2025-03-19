c87d19bd16ea93fbe5e7e395ad8de67c8fe194e3
paolo-licata <paolo_licata@outlook.com>
2025-03-19 16:21:20 +0100

    Fixed routing issue with Hashrouter

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Welcome from "./components/Welcome";
 import LoginForm from "./components/LoginForm";
 import SignupForm from "./components/SignupForm";
